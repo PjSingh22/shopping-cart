9c292798f44940a80b96615f048b5c665c212587
Prabhjot Singh <pjsingh1359@gmail.com>
2022-08-24 14:04:00 -0700

    homepage done

--- src/components/pages/HomePage/Home.jsx
+++ src/components/pages/HomePage/Home.jsx
@@ -1,9 +1,17 @@
 import Shop from "../ShopPage/Shop";
+import { Link } from "react-router-dom";
+
 import './home.css';
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div id='home-container'>
+      <div className='home-header '>
+        <p>About</p>
+      </div>
+      <div className="home-info">
+        <p>Welcome to Valhalla drip, where you can find the finest drip to wear when you are defeating your foes and once you die valorantly in battle you will be the coziest person to ender Valhalla. Shop our finest in season wears today! </p>
+      </div>
+      <Link to='/shop' className="shop-btn">Get Dripped</Link>
     </div>
   );
 };
